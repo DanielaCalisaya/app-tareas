3c59435a68d0cd6ed2a0a455cbd00fc4d577e527
DanielaCalisaya <hdaniela127c@gmail.com>
2022-06-12 13:10:45 -0500

    changes

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import { AppProps } from '../node_modules/next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- pages/notes/index.js
+++
@@ -1,3 +0,0 @@
-export default function Notes () {
-    return <h1>Hola</h1>
-}
